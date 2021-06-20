7aa7da0ccca693509b2ec97a8cff1577cdfd36e4
Matija Osrecki <matija.osrecki@gmail.com>
2021-06-20 19:39:34 +0200

    using chalk

--- 03-node_module_system/notes-app/tempCodeRunnerFile.js
+++ 03-node_module_system/notes-app/tempCodeRunnerFile.js
@@ -1,10 +1,8 @@
 const getNotes = require('./notes.js');
 const chalk = require('chalk');
 
-const notesMsg = getNotes();
-
-console.log(chalk.red.bold.italic(notesMsg));
-console.log(chalk.keyword('green')(notesMsg));
+console.log(chalk.red.bold.italic(getNotes()));
+console.log(chalk.keyword('green')(getNotes()));
 console.log(chalk.keyword('lightgreen').underline('great success!'));
 console.log(
 	chalk.inverse('woah im inversed woah', chalk.bgBlueBright('H.T.B'))
@@ -12,7 +10,6 @@ console.log(
 console.log(chalk.bgCyanBright.black('black panther'));
 console.log(chalk.hex('#BADA55').bold('BADASS😎!'));
 console.log(chalk.rgb(123, 45, 67)('reddish color'));
-console.log(chalk.green.inverse('reddish color'));
 
-const pinkMsg = chalk.bgKeyword('pink').black('puhanic');
-console.log(pinkMsg);
+const pinkMsg = chalk.bgKeyword('pink').black('puhanic')
+console.log(pinkMsg)
