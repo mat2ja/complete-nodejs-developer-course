9411b2eaf6eedc7c795fa7cc663ea8d20dc6db2f
Matija Osrecki <matija.osrecki@gmail.com>
2021-06-20 18:17:28 +0200

    importing our own files

---
+++ 03-node_module_system/notes-app/app.js
@@ -0,0 +1,13 @@
+// const add = require('./utils.js');
+// const sum = add(4, -2);
+// console.log(sum);
+
+//* assignment
+// create a file named notes.js
+// create getNotes fn that returns 'Your notes...'
+// export getNotes fn
+// from app.js, load in and call the fn
+
+const getNotes = require('./notes.js');
+
+console.log(getNotes());
---
+++ 03-node_module_system/notes-app/utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js🧰');
+
+const name = 'Joško';
+
+const add = (a, b) => {
+	return a + b;
+};
+
+// module.exports = name;
+module.exports = add;
