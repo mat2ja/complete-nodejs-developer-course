aa3a52da60146ceea243e0a17e1fe9aaf252caef
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-06 22:35:53 +0200

    yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,48 @@
-const getNotes = require('./notes.js');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes.js');
 
-const notesMsg = getNotes();
-const greenMsg = chalk.bgYellow.black.italic(notesMsg);
+// const cyanMsg = chalk.keyword('cyan').italic;
+// const orangeMsg = chalk.keyword('orange').italic;
+// const redMsg = chalk.keyword('red').italic;
 
-console.log('process.argv :>> ', process.argv);
-console.log(`${greenMsg} for ${process.argv[2] || 'nepoznati boyo'}`);
+yargs.version('1.1.0')
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler() {
+    console.log('Adding a new note!');
+  }
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a new note',
+  handler() {
+    console.log('Removing a note!');
+  }
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler() {
+    console.log('Listing notes!');
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler() {
+    console.log('Reading a note!');
+  }
+})
+
+// yargs.command('read', 'Read a note', () => {
+//   console.log('Reading a note!');
+// })
+
+
+console.log(yargs.argv);
