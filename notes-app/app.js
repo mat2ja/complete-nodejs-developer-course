5aaee98e2263cdfe4621ca5087b74254d92a89c1
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-06 23:06:45 +0200

    adding options

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,47 +2,58 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-// const cyanMsg = chalk.keyword('cyan').italic;
-// const orangeMsg = chalk.keyword('orange').italic;
-// const redMsg = chalk.keyword('red').italic;
+const titleMsg = chalk.bgKeyword('orange').black.italic;
+const bodyMsg = chalk.yellow.italic;
 
-yargs.version('1.1.0')
+yargs.version('1.1.0');
 
+// Add
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler() {
-    console.log('Adding a new note!');
-  }
-})
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note description',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: (argv) => {
+    console.log(titleMsg(` ${argv.title} `));
+    console.log(bodyMsg(argv.body));
+  },
+});
 
+// Remove
 yargs.command({
   command: 'remove',
   describe: 'Remove a new note',
-  handler() {
+  handler(arg) {
     console.log('Removing a note!');
-  }
-})
+  },
+});
 
+// List
 yargs.command({
   command: 'list',
   describe: 'List notes',
   handler() {
     console.log('Listing notes!');
-  }
-})
+  },
+});
 
+// Read
 yargs.command({
   command: 'read',
   describe: 'Read a note',
   handler() {
     console.log('Reading a note!');
-  }
-})
+  },
+});
 
-// yargs.command('read', 'Read a note', () => {
-//   console.log('Reading a note!');
-// })
-
-
-console.log(yargs.argv);
+yargs.parse();
