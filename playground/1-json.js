5e23a6fc6620a222f300d6606ce0c37a6f2fadac
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-07 21:37:28 +0200

    reading a file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 
-const book = {
-  title: 'Ego is the enemy',
-  author: 'Ryan Holiday'
-}
+// const book = {
+//   title: 'Ego is the enemy',
+//   author: 'Ryan Holiday'
+// }
 
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json', bookJSON)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+console.log(dataBuffer.toString());
