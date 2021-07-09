d3004889e5df0571dd7fd22e2c4be13afa4206d9
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-09 21:37:40 +0200

    geocode request

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,23 +7,30 @@ const accentMsg = chalk.yellow.bold;
 
 const [query] = yargs.argv._;
 
-const url = `http://api.weatherstack.com/current?access_key=${process.env.API_KEY}&query=${query}`;
+const weatherUrl = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${query}&limit=1`;
 
-request({ url, json: true }, (err, res, body) => {
-  if ('success' in body && !body.success) {
-    console.log(chalk.red(body.error.info));
-  } else {
-    const { query } = body.request;
-    const {
-      temperature,
-      feelslike,
-      weather_descriptions: [description],
-    } = body.current;
-    console.log(accentMsg(query));
-    console.log(
-      `${description}. It is currently ${accentMsg(
-        temperature
-      )} degrees out. Feels like ${accentMsg(feelslike)} degrees tho.`
-    );
-  }
+// request({ url: weatherUrl, json: true }, (error, response, body) => {
+//   if ('success' in body && !body.success) {
+//     console.log(chalk.red(body.error.info));
+//   } else {
+//     const { query } = body.request;
+//     const {
+//       temperature,
+//       feelslike,
+//       weather_descriptions: [description],
+//     } = body.current;
+//     console.log(accentMsg(query));
+//     console.log(
+//       `${description}. It is currently ${accentMsg(
+//         temperature
+//       )} degrees out. Feels like ${accentMsg(feelslike)} degrees tho.`
+//     );
+//   }
+// });
+
+const geocodeUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.GEOCODE_API_KEY}&limit=1`;
+request({ url: geocodeUrl, json: true }, (error, response, body) => {
+  const [feature] = body.features;
+  const [long, lat] = feature.center;
+  console.log(lat, long);
 });
