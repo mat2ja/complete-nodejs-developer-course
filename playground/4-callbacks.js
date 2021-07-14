5cd9d53722879af8d61686369443136180b2b838
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-14 21:53:14 +0200

    callback challenge

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,17 +1,34 @@
-setTimeout(() => {
-  console.log('2seconds are up');
-}, 2000);
+// setTimeout(() => {
+//   console.log('2seconds are up');
+// }, 2000);
 
-const geocode = (address, callback) => {
+// const geocode = (address, callback) => {
+//   setTimeout(() => {
+//     const data = {
+//       latitude: 0,
+//       longitude: 0,
+//     };
+//     callback(data);
+//   }, 2000);
+// };
+
+// geocode('Philadelphia', (myarg) => {
+//   console.log(myarg);
+// });
+
+//
+//* Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (a, b, callback) => {
   setTimeout(() => {
-    const data = {
-      latitude: 0,
-      longitude: 0,
-    };
-    callback(data);
+    const sum = a + b;
+    callback(sum);
   }, 2000);
 };
 
-geocode('Philadelphia', (myarg) => {
-  console.log(myarg);
-});
+add(1, 4, (sum) => console.log(sum));
