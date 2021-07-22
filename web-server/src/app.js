4ba9279d8afe19d3669671d6e39555b30a2ef3ef
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-22 19:53:34 +0200

    handlebars custom templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -11,7 +11,23 @@ app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index',{
+    title: 'Weather App',
+    name: 'Matijas Osrečkoch'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about',{
+    title: 'About',
+    name: 'Matijaq Osrečque'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help',{
+    helpText: 'Ya need some help',
+  })
 })
 
 app.get('/weather', (req, res) => {
