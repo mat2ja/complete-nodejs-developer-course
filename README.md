67f003d7a2056e5589b5092815411f64fe216611
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-24 16:10:39 +0200

    hbs partials

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # The Complete Node.js Developer Course
 
 [Udemy Link](https://www.udemy.com/course/the-complete-nodejs-developer-course-2/)
+
+    nodemon src/app.js -e js,hbs
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,39 +1,42 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const port = 3000;
+const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
-
-const app = express();
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');3
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
-	console.log(req.app);
 	res.render('index', {
 		title: 'Weather App',
-		name: 'Matijas Osrečkoch',
+		name: 'Matija Osrečki'
 	});
 });
 
 app.get('/about', (req, res) => {
 	res.render('about', {
 		title: 'About',
-		name: 'Matijaq Osrečque',
+		name: 'Matija Osrečki'
 	});
 });
 
 app.get('/help', (req, res) => {
 	res.render('help', {
+		title: 'Help',
 		helpText: 'Ya need some help',
+		name: 'Matija Osrečki'
 	});
 });
 
