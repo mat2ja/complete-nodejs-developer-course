73cfbdb724db93cc4332b84a3cd5d6b45ca242ec
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-25 20:31:56 +0200

    loading and extancting into methods

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,32 +1,36 @@
 console.log('client side javascript 💛');
 
+const clearElement = (elem) => (elem.textContent = '');
+const setElement = (elem, content) => (elem.innerText = content);
+
 const fetchWeather = async (query) => {
 	const url = `http://localhost:3000/weather?address=${query}`;
 
+	clearElement(messageSuccess);
+	clearElement(messageError);
+	setElement(messageLoading, 'Loading...');
 	await fetch(url)
 		.then((res) => res.json())
 		.then((data) => {
+			clearElement(messageLoading);
 			if (data.error) {
-				messageError.textContent = data.error;
-				clearElement(messageSuccess);
+				setElement(messageError, data.error);
 			} else {
-				messageSuccess.innerText = `${data.location}\n${data.forecast}`;
-				clearElement(messageError);
+				setElement(messageSuccess, `${data.location}\n${data.forecast}`);
 			}
 		})
 		.catch((err) => {
-			messageError.textContent = data.error;
-			clearElement(messageSuccess);
+			clearElement(messageLoading);
+			setElement(messageError, err);
 		});
 };
-
-const clearElement = (elem) => (elem.textContent = '');
 
 const weatherForm = document.querySelector('form');
 const search = weatherForm.querySelector('input');
 
 const messageSuccess = document.querySelector('#message-success');
 const messageError = document.querySelector('#message-error');
+const messageLoading = document.querySelector('#message-loading');
 
 weatherForm.addEventListener('submit', async (e) => {
 	e.preventDefault();
