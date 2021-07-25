7105a5dbb94fa099ed08a3ee64c4425d96484645
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-25 17:05:39 +0200

    submitting location on frontend

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,15 +1,27 @@
 console.log('client side javascript 💛');
 
-const url = `http://localhost:3000/weather?address=mali losinj`;
+const fetchWeather = async (query) => {
+	const url = `http://localhost:3000/weather?address=${query}`;
 
-fetch(url)
-	.then((res) => res.json())
-	.then((data) => {
-		if (data.error) {
-			console.log(data.error);
-		} else {
-			console.log(data.location);
-			console.log(data.forecast);
-		}
-	})
-	.catch((err) => console.log(err));
+	await fetch(url)
+		.then((res) => res.json())
+		.then((data) => {
+			if (data.error) {
+				console.log(data.error);
+			} else {
+				console.log(data.location);
+				console.log(data.forecast);
+			}
+		})
+		.catch((err) => console.log(err));
+};
+
+const weatherForm = document.querySelector('form');
+const search = weatherForm.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const location = search.value;
+	fetchWeather(location);
+	location.value = '';
+});
