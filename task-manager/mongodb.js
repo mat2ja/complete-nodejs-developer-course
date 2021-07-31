7361f510484e60ab244e0df977bae10e6b21505b
Matija Osrečki <matija.osrecki@gmail.com>
2021-07-31 18:13:47 +0200

    different ObjectId methods

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,5 +1,5 @@
 // CRUD create read update delete
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 const url = 'mongodb://localhost:27017'
 const dbName = 'task-manager'
@@ -10,6 +10,14 @@ const client = new MongoClient(url, {
     useUnifiedTopology: true
 })
 
+const id = ObjectId()
+console.log(id);
+console.log(id.toString());
+console.log(id.toHexString());
+console.log(id.valueOf());
+console.log(id.getTimestamp());
+console.log(id.inspect())
+
 const mongoConnect = async () => {
     await client.connect()
 
@@ -18,21 +26,21 @@ const mongoConnect = async () => {
     try {
         const tasks = db.collection('tasks')
 
-        const result = await tasks.insertMany([
-            {
-                description: 'fix bike',
-                completed: false
-            },
-            {
-                description: 'setup bitwarden',
-                completed: false
-            },
-            {
-                description: 'study mongo',
-                completed: true
-            },
-        ])
-        console.log(result.insertedIds);
+        // const result = await tasks.insertMany([
+        //     {
+        //         description: 'fix bike',
+        //         completed: false
+        //     },
+        //     {
+        //         description: 'setup bitwarden',
+        //         completed: false
+        //     },
+        //     {
+        //         description: 'study mongo',
+        //         completed: true
+        //     },
+        // ])
+        // console.log(result.insertedIds);
     } catch (error) {
         console.log(error.message);
     }
