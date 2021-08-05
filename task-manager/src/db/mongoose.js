6427602b6c68f1a66837b6c2e7baca4d288f040d
Matija Osrečki <matija.osrecki@gmail.com>
2021-08-05 17:07:54 +0200

    more validation

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -16,20 +16,29 @@ db.once('open', async () => {
 	const userSchema = new mongoose.Schema({
 		name: {
 			type: String,
-			required: [true, 'Name not given'],
+			required: [true, 'Name not provided'],
 			minLength: 2,
 		},
 		age: {
 			type: Number,
+			validate(value) {
+				if (value < 0) {
+					throw new Error('Age must be positive number');
+				}
+				if (!Number.isInteger(value)) {
+					throw new Error('Age must be a whole number');
+				}
+			},
 			min: [12, 'Too young'],
-			max: 120,
+			max: [120, 'Too old'],
 		},
 	});
 
 	const User = new mongoose.model('User', userSchema);
 
 	const user = new User({
-		age: 11,
+		name: 'mirna',
+		age: 24,
 	});
 
 	try {
