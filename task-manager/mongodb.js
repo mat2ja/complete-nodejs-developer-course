74e2c7a3337ce8b047bbbe70ad5648c41de73ac6
Matija Osrečki <matija.osrecki@gmail.com>
2021-08-05 14:22:59 +0200

    update many promise challenge

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -6,42 +6,51 @@ const dbName = 'task-manager';
 
 // creating an instance
 const client = new MongoClient(url, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 const mongoConnect = async () => {
-	await client.connect();
+    await client.connect();
 
-	const db = client.db(dbName);
+    const db = client.db(dbName);
 
-	try {
-		const users = db.collection('users');
-		const tasks = db.collection('tasks');
+    try {
+        const users = db.collection('users');
+        const tasks = db.collection('tasks');
 
-		const updatePromise = tasks.updateMany(
-			{},
-			{
-				$set: {
-					completed: true,
-				},
-			}
-		);
+        try {
+            const { matchedCount, modifiedCount, upsertedCount } = await tasks.updateMany(
+                {},
+                {
+                    $setd: {
+                        completed: true,
+                    },
+                }
+            );
+            console.log(`Matched ${matchedCount}, modified: ${modifiedCount}`);
+            console.log('Upserted:', upsertedCount);
+        } catch (error) {
+            console.log(error.message);
+        }
 
-		const res = await updatePromise;
-		console.log(res);
+        try {
+            const completed = await tasks
+                .find({
+                    completed: true,
+                })
+                .toArray();
+            console.log('Completed tasks', completed);
+        } catch (error) {
+            console.log(error.message);
+        }
 
-		const completed = await tasks
-			.find({
-				completed: true,
-			})
-			.toArray();
-		console.log('Completed tasks', completed);
-	} catch (error) {
-		console.log(error.message);
-	}
+
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 mongoConnect()
-	.catch(console.error)
-	.finally(() => client.close());
+    .catch(console.error)
+    .finally(() => client.close());
