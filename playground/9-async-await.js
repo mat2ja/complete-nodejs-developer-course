fcdd528e2f6193354468b12961a2e78793445a0b
Matija Osrečki <matija.osrecki@gmail.com>
2021-08-12 21:10:04 +0200

    return reject fix :bug:

--- playground/9-async-await.js
+++ playground/9-async-await.js
@@ -1,17 +1,17 @@
 const add = (a, b) => {
     return new Promise((resolve, reject) => {
         if (a < 0 || b < 0) {
-            reject('No negative numbers, bitch!')
+            return reject('No negative numbers, bitch!')
         }
         setTimeout(() => {
             resolve(a + b);
-        }, 1000);
+        }, 3000);
     });
 };
 
 const doWork = async () => {
-    const sum = await add(1, 99)
-    const sum2 = await add(sum, -50)
+    const sum = await add(1, -99)
+    const sum2 = await add(sum, 50)
     const sum3 = await add(sum2, 3)
     return sum3
 }
