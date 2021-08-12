2ab28c3c42d387be3035c13cd5085d01e3a931c3
Matija Osrečki <matija.osrecki@gmail.com>
2021-08-12 21:24:01 +0200

    await challenge

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -2,12 +2,15 @@ import '../src/db/mongoose.js'
 import model from '../src/models/index.js'
 const { Task } = model
 
-Task.findByIdAndRemove('610fdb14f765a316a110f04a')
-    .then(task => {
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndRemove(id)
+    const uncompleted = await Task.countDocuments({ completed: false })
+    return { task, uncompleted }
+}
+
+deleteTaskAndCount('610feb2bb0fc1d60fb5b114b')
+    .then(({ task, uncompleted }) => {
         console.log('Removed: ', task);
-        return Task.countDocuments({ completed: false })
-    })
-    .then(count => {
-        console.log(`There are ${count} tasks yet to be completed`);
+        console.log(`There are ${uncompleted} tasks yet to be completed`);
     })
     .catch(error => console.log(error.message))
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -2,17 +2,6 @@ import '../src/db/mongoose.js'
 import model from '../src/models/index.js'
 const { User } = model
 
-// User.findByIdAndUpdate('610ea3838156b805088ae1c9', { age: 1 })
-//     .then(user => {
-//         console.log(user)
-//         return User.countDocuments({ age: 1 })
-//     })
-//     .then(result => {
-//         console.log(result)
-//     })
-//     .catch(error => console.log(error.message))
-
-
 const updateAgeAndCount = async (id, age) => {
     const user = await User.findByIdAndUpdate(id, { age })
     const count = await User.countDocuments({ age })
