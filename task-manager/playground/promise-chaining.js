664e8001857197376dee5f48338a17dc973d51cc
Matija Osrečki <matija.osrecki@gmail.com>
2021-08-12 21:17:05 +0200

    converting user update and count to async await

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -2,12 +2,27 @@ import '../src/db/mongoose.js'
 import model from '../src/models/index.js'
 const { User } = model
 
-User.findByIdAndUpdate('610ea3838156b805088ae1c9', { age: 1 })
-    .then(user => {
-        console.log(user)
-        return User.countDocuments({ age: 1 })
+// User.findByIdAndUpdate('610ea3838156b805088ae1c9', { age: 1 })
+//     .then(user => {
+//         console.log(user)
+//         return User.countDocuments({ age: 1 })
+//     })
+//     .then(result => {
+//         console.log(result)
+//     })
+//     .catch(error => console.log(error.message))
+
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return { user, count }
+}
+
+updateAgeAndCount('610ea3838156b805088ae1c9', 10)
+    .then(res => {
+        console.log(res);
     })
-    .then(result => {
-        console.log(result)
-    })
-    .catch(error => console.log(error.message))
+    .catch(err => console.log(err.message))
+
+console.log('befoore');
