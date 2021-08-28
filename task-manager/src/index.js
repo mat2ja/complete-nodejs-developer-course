b94dac1f857fa1781982cc1d6fc0019cdbcd5eee
Matija Osrečki <matija.osrecki@gmail.com>
2021-08-28 11:20:05 +0200

    middleware example

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,9 +6,14 @@ import taskRouter from './routers/task.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+	console.log('Time:', new Date())
+	next()
+})
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
+
 
 app.listen(port, () => console.log(`Server is up on port:${port}`));
 
