e551a4d8fd5f4ab2e85303b316fdfe78f2fe2a1f
Matija Osrečki <matija.osrecki@gmail.com>
2021-09-04 16:37:56 +0200

    filter tasks with query params

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,11 +20,20 @@ router.post('/tasks', auth, async (req, res) => {
 
 // Fetch all tasks
 router.get('/tasks', auth, async (req, res) => {
+	const match = {};
+
+	if (req.query.completed) {
+		match.completed = req.query.completed === 'true';
+	}
+
 	try {
-		const tasks = await Task.find({ owner: req.user._id });
-		res.send(tasks);
-		// await req.user.populate('tasks').execPopulate();
-		// res.send(req.user.tasks);
+		await req.user
+			.populate({
+				path: 'tasks',
+				match,
+			})
+			.execPopulate();
+		res.send(req.user.tasks);
 	} catch (error) {
 		res.status(400).send();
 	}
