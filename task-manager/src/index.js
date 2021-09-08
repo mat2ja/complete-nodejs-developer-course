59292dee9946c6fecfb97cc98ee3d518e84f0599
Matija Osrečki <matija.osrecki@gmail.com>
2021-09-08 20:30:56 +0200

    file upload via multer example

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,23 +6,22 @@ import taskRouter from './routers/task.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
+import multer from 'multer';
+
+const upload = multer({
+	dest: 'images/',
+});
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+	res.send();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
 app.listen(port, () => console.log(`Server is up on port:${port}`));
 
-import Task from './models/task.js';
-import User from './models/user.js';
-
-const main = async () => {
-	// const task = await Task.findById('612bc249fb6dd5d67eb600ff');
-	// await task.populate('owner').execPopulate();
-	// console.log(task.owner);
-
-	// const user = await User.findById('612bc1969f1ac1cfd2696108');
-	// await user.populate('tasks').execPopulate();
-	// console.log(user.tasks);
-};
+const main = async () => {};
 
 main();
