a5a81db3d6422d1306f3134de1d8bc8608fe0ba5
Matija Osrečki <matija.osrecki@gmail.com>
2022-07-16 14:43:32 +0200

    Finish test cases challenge

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,10 +1,25 @@
-const { calculateTip } = require('../src/math.js');
+const {
+	calculateTip,
+	fahrenheitToCelsius,
+	celsiusToFahrenheit,
+} = require('../src/math.js');
 
 test('Should calculate total with tip', () => {
 	const total = calculateTip(10, 0.3);
 	expect(total).toBe(13);
 });
+
 test('Should calculate total with default tip', () => {
 	const total = calculateTip(10);
 	expect(total).toBe(12.5);
+});
+
+test('Should convert F to C', () => {
+	const temp = fahrenheitToCelsius(32);
+	expect(temp).toBe(0);
+});
+
+test('Should convert C to F', () => {
+	const temp = celsiusToFahrenheit(0);
+	expect(temp).toBe(32);
 });
