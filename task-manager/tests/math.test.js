ddc479481b4027ba41cd36873e5e8bb60d21cdc8
Matija Osrečki <matija.osrecki@gmail.com>
2022-07-16 14:38:44 +0200

    Write second test

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -2,10 +2,9 @@ const { calculateTip } = require('../src/math.js');
 
 test('Should calculate total with tip', () => {
 	const total = calculateTip(10, 0.3);
-
-	// if (total !== 13) {
-	// 	throw new Error(`Total tip should be 13. Got ${total}`);
-	// }
-
 	expect(total).toBe(13);
+});
+test('Should calculate total with default tip', () => {
+	const total = calculateTip(10);
+	expect(total).toBe(12.5);
 });
